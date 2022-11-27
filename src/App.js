837ad50cbfa974440659d9445648702094e65bcd
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-11-28 02:12:19 +0530

    emmei: no re-rendering happens when we updated the value without useState() hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/1-error-example";
+
+/** 1) useState() Hook:
+ * -> It is a function from React
+ * -> useState() hook is used to preserve our local state (component level state)
+ * -> useState() hook re-renders the component when invoked
+ * -> useState() hook returns an array with 2 elements - a value and a function
+ */
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+
+/** 1) useState() Hook:
+ * -> It is a function from React
+ * -> useState() hook is used to preserve our local state (component level state)
+ * -> useState() hook re-renders the component when invoked
+ * -> useState() hook returns an array with 2 elements - a value and a function
+ */
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Hello World!";
+
+  const handleTitle = () => {
+    title = "Hi! Welcome!!";
+    console.log(title);
+  };
+  return (
+    <section className="container">
+      <h2>{title}</h2>
+      <button className="btn" onClick={handleTitle}>
+        change title
+      </button>
+    </section>
+  );
 };
 
 export default ErrorExample;
