605f60a6509551b1598ce4b20a3346fe831f9dfb
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-01 01:20:46 +0530

    emmei: object as state value and updating using spread operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const person = {
+  //   name: "Kejriwal",
+  //   profession: "CM Delhi",
+  //   education: "IIT",
+  // };
+
+  const [person, setPerson] = useState({
+    name: "Kejriwal",
+    profession: "CM Delhi",
+    education: "IIT",
+  });
+
+  const updatePerson = () => {
+    // we use spread operator to update the object. We first copy the object then update any property we want
+    setPerson({ ...person, education: "IIT Kharagpur" });
+  };
+  return (
+    <article className="container">
+      <h1>{person.name}</h1>
+      <h2>{person.profession}</h2>
+      <h3>{person.education}</h3>
+
+      <br />
+
+      <button className="btn" onClick={updatePerson}>
+        Update Education
+      </button>
+    </article>
+  );
 };
 
 export default UseStateObject;
