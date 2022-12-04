704df73b290b9625c27be4f17873872cdf8b6f6f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-05 01:17:20 +0530

    emmei: we cannot place a react hook within conditional

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -4,14 +4,23 @@ import React, { useState, useEffect } from "react";
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
-
+  /*
   useEffect(() => {
     console.log(value);
     // So, every time the state changes, useEffect() gets invoked!
 
     document.title = `Message ${value}`;
   });
+*/
 
+  /* 2)  We cannot place a react hook into the if/else conditional */
+  /*
+  if (value > 2) {
+    useEffect(() => {
+      console.log(`Value is above ${value}`);
+    });
+  }
+*/
   return (
     <section>
       <h1>{value}</h1>
