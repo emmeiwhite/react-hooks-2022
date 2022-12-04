601d94ee040840348a54df97a9e3dd604969fc5f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-05 01:38:47 +0530

    emmei: dependency list, multiple useEffects

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -13,7 +13,9 @@ const UseEffectBasics = () => {
   });
 */
 
-  /* 2)  We cannot place a react hook into the if/else conditional */
+  /* 2)  We cannot place a react hook into the if/else conditional,
+  
+  This will generate an Error ! This is a rule. */
   /*
   if (value > 2) {
     useEffect(() => {
@@ -21,6 +23,24 @@ const UseEffectBasics = () => {
     });
   }
 */
+
+  /** useEffect() has a second argument as dependency list.
+   *  When the second argument is an Empty Array, useEffect is invoked only once!
+   *  And we pass value or values to that list and each time the value changes
+   *  the component re-renders.
+   *
+   *  We can also use multiple useEffects in our component
+   */
+
+  useEffect(() => {
+    if (value > 2) {
+      console.log(`Value is above ${value}`);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(`The second useEffect is invoked and value is: ${value}`);
+  }, [value]);
   return (
     <section>
       <h1>{value}</h1>
