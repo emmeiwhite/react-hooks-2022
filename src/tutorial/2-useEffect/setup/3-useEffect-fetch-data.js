50518cebb0cc18eb99d1e3cca60d51ce4b0325f8
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-10 20:41:24 +0530

    emmei: fetching API data and rendering UI

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+
+    setUsers(users);
+    console.log(users);
+  };
+
+  // make api call
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      <h2>Github Users</h2>
+
+      <ul className="users">
+        {users.map((user) => {
+          const { id, avatar_url, html_url, login } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h3>{login}</h3>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
