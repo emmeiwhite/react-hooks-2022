c03d889b09fe868868189f50aa646fc244991e24
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-11 15:42:44 +0530

    emmei: multiple returns, loading state

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <h3>loading</h3>;
+  }
   return <h2>multiple returns</h2>;
 };
 
