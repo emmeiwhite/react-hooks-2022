414be84d17993f41ab3eaed6a374872e237e03c7
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-13 01:11:46 +0530

    emmei: handling 404 error

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,13 +1,52 @@
 import React, { useState, useEffect } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://api.github.com/users/QuincyLarsons";
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState({});
 
-  if (loading) {
-    return <h3>loading</h3>;
+  // Now we will retrieve data and show it while we first use a loader
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => {
+        console.log("Response code is:");
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          setIsError(true);
+          setIsLoading(false);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        setUser(user);
+        setIsLoading(false);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("There is an error while fetching data");
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h3>loading ...</h3>;
   }
-  return <h2>multiple returns</h2>;
+
+  if (isError) {
+    return <h3>Something went wrong!!!</h3>;
+  }
+
+  return (
+    <>
+      <article className="user">
+        <img src={user.avatar_url} alt={user.login} />
+        <h3>{user.login}</h3>
+      </article>
+    </>
+  );
 };
 
 export default MultipleReturns;
