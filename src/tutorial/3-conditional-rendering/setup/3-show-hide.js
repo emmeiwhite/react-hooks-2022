70514dd4d0efb5aea3531223d44d5395fd5b74db
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-01-01 14:29:22 +0530

    emmei: cleanup function

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  const [error, setError] = useState(false);
-
+  const [show, setShow] = useState(false);
   return (
-    <main>
-      <button className="btn" onClick={() => setError(!error)}>
-        toggle
+    <main className="container">
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show/Hide
       </button>
-      {error ? <h2>Error ...</h2> : <h2>No Error...</h2>}
+      {show && <Item />}
     </main>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect invoked!");
+    window.addEventListener("resize", checkSize);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  return (
+    <section>
+      <h1>Window:</h1>
+      <h2>Size: {size} px</h2>
+    </section>
   );
 };
 
